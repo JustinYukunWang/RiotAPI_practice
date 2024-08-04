3d3a8f7c402c19ffd3f1a606f47b0430d3e342b4
Justin Wang <90929085+JustinYukunWang@users.noreply.github.com>
2024-08-04 14:37:54 -0400

    Add files via upload

--- player.py
+++ player.py
@@ -1,15 +1,26 @@
 import requests
+import json
 
-API_KEY = "RGAPI-08b6a7e8-7744-4d29-bfe6-9a05808625c1"#needs to be updated everytime we log in
+API_KEY = "RGAPI-e471da40-80fd-4dc5-be05-dbf7dde1b7be"#needs to be updated everytime we log in
 
-
-#fetch the puuid inorder to fetch the player info later
-puuid_fetch_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/Voli%20StormValhir/NA1?api_key=RGAPI-08b6a7e8-7744-4d29-bfe6-9a05808625c1"
+#fetch the puuid inorder to fetch the player info later USING: account: account by riot ID
+puuid_fetch_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/Voli%20StormValhir/NA1?api_key=" + API_KEY
 resp = requests.get(puuid_fetch_url).json()
 puuid = resp['puuid']
 
-#fetching player information using puuid
-player_info_fetch_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/2IPkTfIvLAU3O9UZzwENXmJOXnyFESoDK6tf4OW9_-dvgqc9AyQflo3MqDQzccDxFN8-focd7JbuLQ?api_key=RGAPI-08b6a7e8-7744-4d29-bfe6-9a05808625c1"
+#fetching player information using puuid 
+player_info_fetch_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/2IPkTfIvLAU3O9UZzwENXmJOXnyFESoDK6tf4OW9_-dvgqc9AyQflo3MqDQzccDxFN8-focd7JbuLQ?api_key=" + API_KEY
 player_info = requests.get(player_info_fetch_url).json()
 
-print(player_info)
+
+player_matchHistory_fetch_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=20&api_key=" +  API_KEY
+matchData = requests.get(player_matchHistory_fetch_url).json()
+
+
+
+
+
+with open("matchData.txt", "w") as outfile:
+    outfile.write("\n".join(matchData))
+    outfile.close()
+
